51bd38b30c99286956dd778eb4a890448ea546b2
murmaeuschen <chumako@gmail.com>
2013-02-03 15:08:21 +0200

    first

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,7 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :order
-  attr_accessible :price_per_line, :quantity, :item_id
+  attr_accessible :price_per_line, 
+                  :quantity,
+                  :item_id
 end
